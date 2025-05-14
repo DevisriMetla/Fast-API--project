bc6203a46a0002bb6f95e5ce4a5125b689d1dbd9
devisri <devisri.metla@gmail.com>
2025-05-14 12:30:45 +0530

    Implemented

--- app/main.py
+++ app/main.py
@@ -8,7 +8,8 @@ from app.database import SessionLocal, get_db
 models.Base.metadata.create_all(bind=database.engine)
 app = FastAPI()
 app.include_router(auth.router)
-app.include_router(users.router)
+app.include_routers(users.router)
+
 
 
 

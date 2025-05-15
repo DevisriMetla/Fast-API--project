3b25724444997cda8dd834a0bcb664c874004727
DevisriMetla <devisri.metla@kleza.io>
2025-05-15 11:18:56 +0530

    Implemented Fast API

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ from app.database import SessionLocal, get_db
 models.Base.metadata.create_all(bind=database.engine)
 app = FastAPI()
 app.include_router(auth.router)
-app.include_routers(users.router)
+app.include_router(users.router)
 
 
 
